bc7986b2a393d9685147f3d34c933d247fc1c471
smymrr <mochamadsergian@gmail.com>
2026-08-25 22:53:42 +0700

    Add hit detection

--- scripts/player/attack_component.gd
+++ scripts/player/attack_component.gd
@@ -8,14 +8,16 @@ extends Node
 
 @onready var hitbox: Area2D = %AttackHitbox
 @onready var hitbox_visual: ColorRect = %AttackHitbox/ColorRect
-@onready var hitbox_offset: Vector2
 
+var hitbox_offset: Vector2
 var can_attack: bool = true
+var _already_hit: Array[Node2D] = []
 
 func _ready() -> void:
 	hitbox.monitoring = false
-	#hitbox_visual.modulate.a = 0.0  # invisible at start
+	hitbox_visual.modulate.a = 0.0  # invisible at start
 	hitbox_offset = hitbox.position
+	hitbox.body_entered.connect(_on_attack_hitbox_body_entered)
 
 func try_attack(input_direction: Vector2) -> bool:
 	if not can_attack or input_direction == Vector2.ZERO:
@@ -25,16 +27,17 @@ func try_attack(input_direction: Vector2) -> bool:
 
 func _perform_attack(input_direction: Vector2) -> void:
 	can_attack = false
+	_already_hit.clear()
 	
 	get_parent().rotation = input_direction.angle()
 	_trigger_attack_animation()
 	_trigger_attack_sfx()
 
 	hitbox.monitoring = true
-	#hitbox_visual.modulate.a  = 0.4 # show it
+	hitbox_visual.modulate.a  = 0.4 # show it
 	await get_tree().create_timer(attack_duration).timeout
 	hitbox.monitoring = false
-	#hitbox_visual.modulate.a = 0.0                    # hide it again
+	hitbox_visual.modulate.a = 0.0                    # hide it again
 
 	var remaining := attack_cooldown - attack_duration
 	if remaining > 0:
@@ -50,3 +53,10 @@ func _trigger_attack_animation() -> void:
 
 func _trigger_attack_sfx() -> void:
 	pass
+
+func _on_attack_hitbox_body_entered(body: Node2D) -> void:
+	if body in _already_hit:
+		return
+	
+	_already_hit.append(body)
+	print("Hit")
