a698e8013f59d61a7b962dc9c841cf18cb39dd66
Sergian <mochamadsergian@gmail.com>
2026-09-04 14:37:15 +0700

    add idle and walking animation to player, refactor player dash functions

--- scripts/player/movement_component.gd
+++ scripts/player/movement_component.gd
@@ -4,9 +4,14 @@ extends Node
 # Movement Variables
 @export var speed: float = PlayerData.base_speed
 
-@export var dash_speed: float = 900.0
+@export var dash_speed: float = 200.0
 @export var dash_duration: float = 0.2
 @export var dash_cooldown: float = 2.0
+
+@onready var sprite: AnimatedSprite2D = $"../Sprite"
+
+var last_facing_direction: Vector2 = Vector2.RIGHT
+var body: CharacterBody2D
 
 # States
 var is_dashing: bool = false
@@ -15,16 +20,23 @@ var can_dash: bool = true
 signal dash_started
 signal dash_finished
 
+func setup(player_body: CharacterBody2D):
+	body = player_body
+
 # Perhitungan velocity pergerakan normal
 func get_velocity(input_direction: Vector2) -> Vector2:
 	return input_direction * speed
 
-# TODO: Ganti logika ini sepenuhnya, ganti dengan animasi saja (ACA)
-func get_rotation_degrees(direction: Vector2) -> float:
-	return rad_to_deg(DirectionUtils.snap_to_cardinal(direction).angle())
-	
 # Dash Mechanism
-func start_dash(direction: Vector2) -> Vector2:
+func process_dash(input_direction: Vector2) -> void:
+	if Input.is_action_just_pressed("dash") and can_dash:
+		var target_dir = input_direction if input_direction != Vector2.ZERO else last_facing_direction
+		var dash_velocity = _start_dash(target_dir)
+		
+		if dash_velocity != Vector2.ZERO:
+			body.velocity = dash_velocity
+
+func _start_dash(direction: Vector2) -> Vector2:
 	if not can_dash or direction == Vector2.ZERO:
 		return Vector2.ZERO
 
@@ -39,6 +51,26 @@ func start_dash(direction: Vector2) -> Vector2:
 	get_tree().create_timer(dash_cooldown).timeout.connect(_on_cooldown_timeout)
 
 	return direction.normalized() * dash_speed
+
+
+# Animation Process
+func process_animation(direction: Vector2) -> void:
+	if body.velocity != Vector2.ZERO:
+		_play_animation("walk", direction)
+	else:
+		_play_animation("idle",  direction)
+
+func _play_animation(prefix: String, direction: Vector2) -> void:
+	if abs(direction.x) >= abs(direction.y):
+		if direction.x > 0:
+			sprite.play(prefix + "_right")
+		else:
+			sprite.play(prefix + "_left")
+	else:
+		if direction.y > 0:
+			sprite.play(prefix + "_down")
+		else:
+			sprite.play(prefix + "_up")
 
 # Pergantian states
 func _on_dash_duration_timeout() -> void:
